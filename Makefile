a882dcd9f9a6a2683b567457149514cea5a3ce0a
thisisaaronland <thisisaaronland@localhost>
2026-01-30 11:58:32 -0800

    snapshot: build protos

--- Makefile
+++ Makefile
@@ -2,3 +2,14 @@
 
 macos:
 	xcodebuild -destination 'platform=macOS' -scheme docent -configuration Release
+
+# https://swiftpackageindex.com/grpc/grpc-swift-protobuf/2.1.2/documentation/grpcprotobuf/code-generation-with-protoc
+
+protoc:
+	# rm Protos/docent_service/*.swift
+	protoc \
+		--swift_out=. \
+		--swift_opt=Visibility=Public \
+		--grpc-swift-2_out=. \
+		--grpc-swift-2_opt=Visibility=Public \
+		Protos/docent_service/docent_service.proto
--- Package.swift
+++ Package.swift
@@ -23,6 +23,9 @@ let package = Package(
         .package(url: "https://github.com/ml-explore/mlx-swift-lm/", branch: "main"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.7.0"),
         .package(url: "https://github.com/apple/swift-log", from: "1.9.1"),
+        .package(url: "https://github.com/grpc/grpc-swift-2.git", from: "2.0.0"),
+        .package(url: "https://github.com/grpc/grpc-swift-protobuf.git", from: "2.0.0"),
+        .package(url: "https://github.com/grpc/grpc-swift-nio-transport.git", from: "2.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -47,7 +50,10 @@ let package = Package(
                 "WallLabel",
                 "Summarizer",
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "Logging", package: "swift-log")
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "GRPCCore", package: "grpc-swift-2"),
+                .product(name: "GRPCNIOTransportHTTP2", package: "grpc-swift-nio-transport"),
+                .product(name: "GRPCProtobuf", package: "grpc-swift-protobuf"),
             ]
         ),
     ]
