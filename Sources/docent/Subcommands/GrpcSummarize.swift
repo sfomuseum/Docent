74f508b4322e081d23d9a353506024dd5a55be40
Aaron Straup Cope <thisisaaronland@users.noreply.github.com>
2026-02-02 14:08:16 -0800

    ensure TLS connections for gRPC clients; update MLX stuff to allow for custom download folder (#8)
    Co-authored-by: thisisaaronland <thisisaaronland@localhost>

--- Sources/docent/Subcommands/GrpcSummarize.swift
+++ Sources/docent/Subcommands/GrpcSummarize.swift
@@ -16,14 +16,17 @@ struct GrpcSummarize: AsyncParsableCommand {
     @Option(help: "The port for the gRPC server.")
     var port: Int = 8080
     
-    @Option(help: "Enable verbose logging")
-    var verbose: Bool = false
+    @Option(help: "The TLS certificate chain to use for encrypted connections.")
+    var tls_certificate: String = ""
 
     @Option(help: "The maximum length of the summary text. This value may be overridden by the gRPC server.")
     var max_length: Int = 77
     
     @Option(help: "The maximum number of attempts to make summarizing the text to be no longer than the value of the --max_length flag. This value may be overridden by the gRPC server.")
     var max_retries: Int = 1
+    
+    @Option(help: "Enable verbose logging")
+    var verbose: Bool = false
     
     @Argument(help: "The text to operate on. If \"-\" then data is read from STDIN. If the first argument is a valid path to a local file then the text of that file will be used. Otherwise all remaining arguments will be concatenated (with a space) and used as the text to process.")
     var args: [String]
@@ -36,11 +39,22 @@ struct GrpcSummarize: AsyncParsableCommand {
             logger.logLevel = .debug
         }
         
+        var transportSecurity = HTTP2ClientTransport.Posix.TransportSecurity.plaintext
+
+        if tls_certificate != ""  {
+            
+            let certSource:  TLSConfig.CertificateSource   = .file(path: tls_certificate, format: .pem)
+            
+             transportSecurity = HTTP2ClientTransport.Posix.TransportSecurity.tls { config in
+                config.certificateChain = [ certSource ]
+            }
+        }
+        
         try await withGRPCClient(
             
             transport: .http2NIOPosix(
-                target: .ipv4(address: self.host, port: self.port),
-                transportSecurity: .plaintext
+                target: .dns(host: self.host, port: self.port),
+                transportSecurity: transportSecurity
             )
             
         ) { client in
